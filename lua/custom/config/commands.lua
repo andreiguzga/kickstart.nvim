6b5ebc28aeb94547b5f98c99539d1385deb5a7f4
Andrei Guzga <shhican@gmail.com>
2024-09-04 14:31:04 +0300

    Added commands.lua and nvim-ufo for folding

---
+++ lua/custom/config/commands.lua
@@ -0,0 +1,10 @@
+-- Use an autocommand to set the fold column for all buffers
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.foldcolumn = '1'
+    vim.opt_local.foldenable = true
+    vim.opt_local.foldlevel = 99
+    vim.opt_local.foldlevelstart = 99
+  end,
+})
