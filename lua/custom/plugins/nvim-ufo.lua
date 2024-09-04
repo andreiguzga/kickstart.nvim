da66e23c9fa6b24f6f882a3fedb6d63c093c4cc9
Andrei Guzga <shhican@gmail.com>
2024-09-04 14:38:38 +0300

    Added fold peek keymap for nvim-ufo

--- lua/custom/plugins/nvim-ufo.lua
+++ lua/custom/plugins/nvim-ufo.lua
@@ -11,8 +11,14 @@ return {
       vim.o.foldenable = true
 
       -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+      vim.keymap.set('n', 'zK', function()
+        local winid = require('ufo').peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { desc = 'Peek folded lines under cursor' })
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.foldingRange = {
