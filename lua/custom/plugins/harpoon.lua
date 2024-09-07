e14ae3b4c1bcd67391a04b9cfe247d7a1276bd8e
Andrei Guzga <shhican@gmail.com>
2024-09-07 14:09:11 +0300

    More keybindings to Harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,6 +9,17 @@ return {
 
       local conf = require('telescope.config').values
       local function toggle_telescope(harpoon_files)
+        local finder = function()
+          local paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(paths, item.value)
+          end
+
+          return require('telescope.finders').new_table {
+            results = paths,
+          }
+        end
+
         local file_paths = {}
         for _, item in ipairs(harpoon_files.items) do
           table.insert(file_paths, item.value)
@@ -17,11 +28,26 @@ return {
         require('telescope.pickers')
           .new({}, {
             prompt_title = 'Harpoon',
-            finder = require('telescope.finders').new_table {
-              results = file_paths,
-            },
+            finder = finder(),
             previewer = conf.file_previewer {},
             sorter = conf.generic_sorter {},
+            layout_config = {
+              height = 0.4,
+              width = 0.5,
+              prompt_position = 'top',
+              preview_cutoff = 120,
+            },
+            attach_mappings = function(prompt_bufnr, map)
+              map('i', '<C-d>', function()
+                local state = require 'telescope.actions.state'
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+              end)
+              return true
+            end,
           })
           :find()
       end
@@ -30,17 +56,46 @@ return {
         toggle_telescope(harpoon:list())
       end, { desc = 'Open harpoon window' })
 
-      vim.keymap.set('n', '<leader>a', function()
+      vim.keymap.set('n', '<leader>h', function()
         harpoon:list():add()
-      end)
+      end, { desc = 'Add current buffer to harpoon' })
+
+      vim.keymap.set('n', '<leader>1', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon #1' })
+      vim.keymap.set('n', '<leader>2', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon #2' })
+      vim.keymap.set('n', '<leader>3', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon #3' })
+      vim.keymap.set('n', '<leader>4', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon #4' })
 
       -- Toggle previous & next buffers stored within Harpoon list
       vim.keymap.set('n', '<C-S-P>', function()
         harpoon:list():prev()
-      end)
+      end, { desc = 'Select previous buffer in harpoon' })
       vim.keymap.set('n', '<C-S-N>', function()
         harpoon:list():next()
-      end)
+      end, { desc = 'Select next buffer in harpoon' })
+
+      harpoon:extend {
+        UI_CREATE = function(cx)
+          vim.keymap.set('n', '<C-v>', function()
+            harpoon.ui:select_menu_item { vsplit = true }
+          end, { buffer = cx.bufnr })
+
+          vim.keymap.set('n', '<C-x>', function()
+            harpoon.ui:select_menu_item { split = true }
+          end, { buffer = cx.bufnr })
+
+          vim.keymap.set('n', '<C-t>', function()
+            harpoon.ui:select_menu_item { tabedit = true }
+          end, { buffer = cx.bufnr })
+        end,
+      }
     end,
   },
 }
