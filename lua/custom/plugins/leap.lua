35f19679d954b1ad76cb9c26b1bcb55bbd36d1fd
Andrei Guzga <shhican@gmail.com>
2024-09-08 13:32:42 +0300

    Fixed issue with keybindings in leap plugin

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -5,9 +5,9 @@ return {
       'tpope/vim-repeat',
     },
     config = function()
-      vim.keymap.set({ 'n', 'x', 'o' }, 'l', '<Plug>(leap-forward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'L', '<Plug>(leap-backward)')
-      vim.keymap.set({ 'n', 'x', 'o' }, 'gl', '<Plug>(leap-from-window)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'M', '<Plug>(leap-backward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'm', '<Plug>(leap-forward)')
+      vim.keymap.set({ 'n', 'x', 'o' }, 'gm', '<Plug>(leap-from-window)')
     end,
   },
 }
